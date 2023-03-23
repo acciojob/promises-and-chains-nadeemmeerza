788e885fafdcd0d5fb825d288f9af2cd87c02cf9
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-03-23 21:16:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 //your JS code here. If required.
 let btn = document.getElementById("btn");
-btn.addEventListener("click" (e)=>{
+
+btn.addEventListener("click", (e)=>{
 	let age = document.getElementById("age").value;
 	let name = document.getElementById("name").value;
-	return new Promise(res, rej){
-		if(age > 18)
-			res(`Welcome, ${name}. You can vote.`);
-		else
-			rej(`Oh sorry ${name}. You aren't old enough`);
-	}
-}).then(mes=>{
-	alert(mes);
-}).catch(err=>{
-	alert(err);
+	return new Promise((res, rej)=>{		
+		setTimeout(()=>{
+			console.log("timeout");
+			if(age > 18)	
+				
+			      res(`Welcome, ${name}. You can vote.`); 				
+		   else			
+			      rej(`Oh sorry ${name}. You aren't old enough`);	
+	}, 4000) ;  
+	       })
+		.then(mes=>{			
+	           alert(mes);	
+			  
+         })
+		.catch(err=>{
+	         alert(err);
+            });		
 })
+	
+	
+		
